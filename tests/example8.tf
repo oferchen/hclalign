ef56cc4f6274c83b87a1eaa7de37827333b8540d
Ofer Chen <oferchen@gmail.com>
2024-04-12 18:58:03 +0200

    revised example8.tf again for more corner cases

--- tests/example8.tf
+++ tests/example8.tf
@@ -1,9 +1,13 @@
 // example8.tf
 variable "example_var_inline_comment" { # Problematic corner case inline comment
-default = "corner { } case"
+default = "corner { } case"                                                                        // Corner case? # what about this? /* about now? */
 /* multi line
 comment */
   description = "An example variable with an inline comment" # Just another inline 
 // different tyoe of comment
-type = string
+                                                                         type = string
+validation = { // this is screwed up
+                                    # how about now?
+key = value  # and this one?
+}
 }
