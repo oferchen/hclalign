142b0c2d8cf30e69507c1e1754d4cc05a823db50
Ofer Chen <oferchen@gmail.com>
2024-04-16 21:54:30 +0200

    missing newline

--- tests/example8.tf
+++ tests/example8.tf
@@ -1,13 +1,11 @@
-// example8.tf
-variable "example_var_inline_comment" { # Problematic corner case inline comment
-default = "corner { } case"                                                                        // Corner case? # what about this? /* about now? */
-/* multi line
-comment */
-  description = "An example variable with an inline comment" # Just another inline 
-// different tyoe of comment
-                                                                         type = string
-validation = { // this is screwed up
-                                    # how about now?
-key = value  # and this one?
+variable "example_var_inline_comment" { default = "corner { } case" // example8.tf
+  # Problematic corner case inline comment
+  // Corner case? # what about this? /* about now? */
+  /* multi line
+comment */ description = "An example variable with an inline comment" type = string # Just another inline 
+  // different tyoe of comment
+  validation = { key = value } // this is screwed up
+  # how about now?
+  # and this one?
 }
-}
+
