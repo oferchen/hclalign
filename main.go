fa872b95987bc8e3cc6dfa93137f4053ba2ac68c
Ofer Chen <oferchen@gmail.com>
2025-08-28 13:37:08 +0200

    refactor: streamline main exit handling

--- main.go
+++ main.go
@@ -3,9 +3,6 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/oferchen/hclalign/cli"
 	"github.com/oferchen/hclalign/config"
 	"github.com/spf13/cobra"
@@ -13,7 +10,7 @@ import (
 
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "hcl_align [target file or directory]",
+		Use:   "hclalign [target file or directory]",
 		Short: "Aligns HCL files based on given criteria",
 		Args:  cobra.MinimumNArgs(1),
 		RunE:  cli.RunE,
@@ -22,8 +19,5 @@ func main() {
 	rootCmd.Flags().StringSliceP("criteria", "c", config.DefaultCriteria, "List of file criteria to align")
 	rootCmd.Flags().StringSliceP("order", "o", config.DefaultOrder, "Comma-separated list of the order of variable block fields")
 
-	if err := rootCmd.Execute(); err != nil {
-		log.Fatalf("Error: %v", err)
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
