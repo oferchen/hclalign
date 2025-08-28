96fe31e8c2665b96a0fa5da28f8cb2318206582f
Ofer Chen <oferchen@gmail.com>
2025-08-28 23:45:50 +0200

    Add golden test for template expressions

---
+++ tests/cases/templates/in.tf
@@ -0,0 +1,13 @@
+variable "interpolation" {
+  default = "${var.foo}"
+  type    = string
+}
+
+variable "directive" {
+  default = <<-EOT
+%{if var.bar}
+${var.bar}
+%{endif}
+EOT
+  type = string
+}
