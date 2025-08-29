3e0111cae3852e6f0a83c0eb61c4a7c8d6ca8940
Ofer Chen <oferchen@gmail.com>
2025-08-29 20:57:14 +0200

    Prioritize alias in provider blocks

--- tests/cases/provider/aligned.tf
+++ tests/cases/provider/aligned.tf
@@ -1,4 +1,15 @@
 provider "aws" {
-  alias  = "east"
+  # alias comment
+  alias = "east"
+  # access key comment
+  access_key = "foo"
+  # region comment
   region = "us-east-1"
+  # secret key comment
+  secret_key = "bar"
+
+  # assume role block
+  assume_role {
+    role_arn = "arn:aws:iam::123456789012:role/test"
+  }
 }
