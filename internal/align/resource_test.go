4e078c4066823490e91c4c21b2b2fdffd544a6ad
Ofer Chen <oferchen@gmail.com>
2025-08-30 14:18:47 +0200

    test: order lifecycle and provisioners

--- internal/align/resource_test.go
+++ internal/align/resource_test.go
@@ -112,3 +112,58 @@ func TestMetaArgsOrder(t *testing.T) {
 }`
 	require.Equal(t, exp, got)
 }
+
+func TestLifecycleProvisionerOrder(t *testing.T) {
+	src := []byte(`resource "test_thing" "ex" {
+  baz        = 3
+  provisioner "local-exec" {}
+  for_each   = {}
+  foo        = 1
+  lifecycle {
+    prevent_destroy = true
+  }
+  bar        = 2
+  random     = 4
+  provider   = "p"
+  count      = 1
+  depends_on = []
+  provisioner "remote-exec" {}
+}`)
+
+	file, diags := hclwrite.ParseConfig(src, "in.tf", hcl.InitialPos)
+	require.False(t, diags.HasErrors())
+
+	sch := &alignpkg.Schema{
+		Required: map[string]struct{}{"foo": {}},
+		Optional: map[string]struct{}{"bar": {}},
+		Computed: map[string]struct{}{"baz": {}},
+		Meta:     map[string]struct{}{"provider": {}, "depends_on": {}, "count": {}, "for_each": {}},
+	}
+	schemas := map[string]*alignpkg.Schema{"test_thing": sch}
+
+	require.NoError(t, alignpkg.Apply(file, &alignpkg.Options{Schemas: schemas}))
+
+	got := string(file.Bytes())
+	exp := `resource "test_thing" "ex" {
+  provider   = "p"
+  count      = 1
+  for_each   = {}
+  depends_on = []
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  provisioner "local-exec" {}
+
+  provisioner "remote-exec" {}
+  foo    = 1
+  bar    = 2
+  baz    = 3
+  random = 4
+}`
+	require.Equal(t, exp, got)
+
+	require.NoError(t, alignpkg.Apply(file, &alignpkg.Options{Schemas: schemas}))
+	require.Equal(t, exp, string(file.Bytes()))
+}
