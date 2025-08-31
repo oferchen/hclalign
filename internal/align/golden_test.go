4429d491bc8b86cd584f47ab0533d90258c890f0
Ofer Chen <oferchen@gmail.com>
2025-08-31 14:05:12 +0200

    Handle ephemeral variable attribute

--- internal/align/golden_test.go
+++ internal/align/golden_test.go
@@ -62,19 +62,9 @@ func TestGolden(t *testing.T) {
 			if err != nil {
 				t.Fatalf("format input: %v", err)
 			}
-
-			fmtBytes, _, err := terraformfmt.Format(context.Background(), inBytes, inPath, string(terraformfmt.StrategyGo))
-			if err != nil {
-				t.Fatalf("format input: %v", err)
-			}
 			hadNewline := len(inBytes) > 0 && inBytes[len(inBytes)-1] == '\n'
 			if !hadNewline && len(fmtBytes) > 0 && fmtBytes[len(fmtBytes)-1] == '\n' {
 				fmtBytes = fmtBytes[:len(fmtBytes)-1]
-			}
-
-			againFmt, _, err := terraformfmt.Format(context.Background(), fmtBytes, inPath, string(terraformfmt.StrategyGo))
-			if err != nil {
-				t.Fatalf("format fmt: %v", err)
 			}
 
 			againFmt, _, err := terraformfmt.Format(context.Background(), fmtBytes, inPath, string(terraformfmt.StrategyGo))
