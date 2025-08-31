1718c71bab92c46643fa9207e0d250528f90344f
Ofer Chen <oferchen@gmail.com>
2025-08-31 13:17:30 +0200

    cache terraform lookup

--- internal/fmt/runner.go
+++ internal/fmt/runner.go
@@ -4,16 +4,27 @@ package terraformfmt
 import (
 	"context"
 	"os/exec"
+	"sync"
 
 	"github.com/oferchen/hclalign/formatter"
 	internalfs "github.com/oferchen/hclalign/internal/fs"
 )
 
+var terraformPath string
+var terraformPathOnce sync.Once
+
+func terraformBinary() string {
+	terraformPathOnce.Do(func() {
+		terraformPath, _ = exec.LookPath("terraform")
+	})
+	return terraformPath
+}
+
 func Run(ctx context.Context, src []byte) ([]byte, internalfs.Hints, error) {
 	if err := ctx.Err(); err != nil {
 		return nil, internalfs.Hints{}, err
 	}
-	if _, err := exec.LookPath("terraform"); err == nil {
+	if terraformBinary() != "" {
 		return formatBinary(ctx, src)
 	}
 	return formatter.Format(src, "")
