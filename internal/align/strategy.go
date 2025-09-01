e08efc2f0169934f53d03e17c2f4a93aa01f3e74
Ofer Chen <oferchen@gmail.com>
2025-09-01 20:18:45 +0200

    feat: retain schema attribute order

--- internal/align/strategy.go
+++ internal/align/strategy.go
@@ -1,7 +1,11 @@
 // internal/align/strategy.go
 package align
 
-import "github.com/hashicorp/hcl/v2/hclwrite"
+import (
+	"strings"
+
+	"github.com/hashicorp/hcl/v2/hclwrite"
+)
 
 type Options struct {
 	Order   []string
@@ -17,6 +21,13 @@ type Schema struct {
 	Optional map[string]struct{}
 	Computed map[string]struct{}
 	Meta     map[string]struct{}
+
+	RequiredOrder []string
+	OptionalOrder []string
+	ComputedOrder []string
+
+	Blocks      map[string]*Schema
+	BlocksOrder []string
 }
 
 type Strategy interface {
@@ -40,27 +51,40 @@ func Apply(file *hclwrite.File, opts *Options) error {
 
 func applyBody(body *hclwrite.Body, opts *Options) error {
 	for _, b := range body.Blocks() {
+		sub := *opts
+		sub.Schema = nil
+		if len(b.Labels()) > 0 && opts.Schemas != nil {
+			typ := b.Labels()[0]
+			if s, ok := opts.Schemas[typ]; ok {
+				sub.Schema = s
+			} else {
+				for k, s := range opts.Schemas {
+					if strings.HasSuffix(k, "/"+typ) {
+						sub.Schema = s
+						break
+					}
+				}
+			}
+		} else if opts.Schema != nil {
+			if s, ok := opts.Schema.Blocks[b.Type()]; ok {
+				sub.Schema = s
+			}
+		}
+
 		if strategy, ok := registry[b.Type()]; ok {
 			if opts.Types != nil {
 				if _, ok := opts.Types[b.Type()]; !ok {
-					if err := applyBody(b.Body(), opts); err != nil {
+					if err := applyBody(b.Body(), &sub); err != nil {
 						return err
 					}
 					continue
-				}
-			}
-			sub := *opts
-			sub.Schema = nil
-			if len(b.Labels()) > 0 && opts.Schemas != nil {
-				if s, ok := opts.Schemas[b.Labels()[0]]; ok {
-					sub.Schema = s
 				}
 			}
 			if err := strategy.Align(b, &sub); err != nil {
 				return err
 			}
 		}
-		if err := applyBody(b.Body(), opts); err != nil {
+		if err := applyBody(b.Body(), &sub); err != nil {
 			return err
 		}
 	}
